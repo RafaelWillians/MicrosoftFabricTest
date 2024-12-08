28f3cd5daf6a17cc969306292537e1f8d6cb0d1e
Rafael Silva Willians <rafael.willians@outlook.com>
2024-12-08 19:05:58 -0300

    Adicionada info no notebook

--- deltaTables/DeltaTablesNotebook.Notebook/notebook-content.py
+++ deltaTables/DeltaTablesNotebook.Notebook/notebook-content.py
@@ -26,6 +26,11 @@
 # Use this notebook to explore Delta Lake functionality
 
 
+# MARKDOWN ********************
+
+# ## Create a schema
+# The code below defines a schema and shows the dataframe from "products.csv" file.
+
 # CELL ********************
 
 from pyspark.sql.types import StructType, IntegerType, StringType, DoubleType
@@ -39,7 +44,7 @@ schema = StructType() \
 
 df = spark.read.format("csv").option("header","true").schema(schema).load("Files/products/products.csv")
 
-# df now is a Spark DataFrame containing CSV data from "Fiels/products/products.csv"
+# df now is a Spark DataFrame containing CSV data from "Files/products/products.csv"
 
 display(df)
 
@@ -47,5 +52,80 @@ display(df)
 
 # META {
 # META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# ## Create managed and external Delta tables
+
+# CELL ********************
+
+# Creates a managed Delta table
+df.write.format("delta").saveAsTable("managed_products")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Sets the file path and creates a external Delta table
+dfPath = "abfss://8fb3e71f-f4ce-4f5f-a159-4da9b4cfe6f0@onelake.dfs.fabric.microsoft.com/a4042216-07c2-46e2-840a-5a5414a9b6fd/Files/external_products"
+
+df.write.format("delta").saveAsTable("external_products", path=dfPath)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# Shows the detail of both tables. 
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DESCRIBE FORMATTED managed_products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DESCRIBE FORMATTED external_products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# Then drop both tables, to indicate that all files in the managed table were deleted, while the external table was deleted but the parquet and log files were kept.
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC DROP TABLE managed_products;
+# MAGIC DROP TABLE external_products;
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
 # META   "language_group": "synapse_pyspark"
 # META }
