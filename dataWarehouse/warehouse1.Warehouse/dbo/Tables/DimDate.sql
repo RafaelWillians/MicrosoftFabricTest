864e3863ba825895006d2b99d2f923be08db228c
Rafael Silva Willians <rafael.willians@outlook.com>
2024-12-09 16:37:55 -0300

    Criado report usando data warehouse

---
+++ dataWarehouse/warehouse1.Warehouse/dbo/Tables/DimDate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[DimDate] (
+
+	[DateKey] int NOT NULL, 
+	[DateAltKey] date NOT NULL, 
+	[DayOfWeek] int NOT NULL, 
+	[WeekDayName] varchar(10) NULL, 
+	[DayOfMonth] int NOT NULL, 
+	[Month] int NOT NULL, 
+	[MonthName] varchar(12) NULL, 
+	[Year] int NOT NULL
+);
+
+
+GO
+ALTER TABLE [dbo].[DimDate] ADD CONSTRAINT UQ_57100eb5_b44c_4871_9f44_a92c65937a46 unique NONCLUSTERED ([DateKey]);
